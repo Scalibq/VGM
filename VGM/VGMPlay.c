52053d8ee45072aeacfcffc9742581355d8869f5
Scali <none@none>
2018-02-05 13:37:46 +0100

    First semi-working version of DRO preprocessing and playback. Timing is somewhat haphazard...

--- VGM/VGMPlay.c
+++ VGM/VGMPlay.c
@@ -2049,7 +2049,214 @@ void PreProcessMIDI(FILE* pFile, const char* pOutFile)
 
 void PreProcessDRO(FILE* pFile, const char* pOutFile)
 {
+	DROHeader header;
+	FILE* pOut;
+	uint8_t conversionTable[128];
+	uint32_t lengthPairs = 0;
+	uint32_t delay = 0, totalDelay = 0;
+	uint32_t size;
+	uint16_t i, j, firstDelay;
 	
+	fread(&header, sizeof(header), 1, pFile);
+	
+	printf("DRO file version %u.%u\n", header.versionHigh, header.versionLow);
+	if (!((header.versionHigh == 2) && (header.versionLow == 0)))
+	{
+		printf("Unsupported version!\n");
+		return;
+	}
+	
+	switch (header.hardware)
+	{
+		case 0:
+			printf("OPL2\n");
+			preHeader.nrOfYM3812 = 1;
+			break;
+		case 1:
+			printf("Dual OPL2\n");
+			preHeader.nrOfYM3812 = 2;
+			break;
+		case 2:
+			printf("OPL3\n");
+			preHeader.nrOfYMF262 = 1;
+			break;
+	}
+	
+	// Load conversion table
+	fread(conversionTable, header.conversionTableSize, 1, pFile);
+	
+	pOut = fopen(pOutFile, "wb");
+
+	printf("# SN76479: %u\n", preHeader.nrOfSN76489);
+	printf("# SAA1099: %u\n", preHeader.nrOfSAA1099);
+	printf("# AY8930: %u\n", preHeader.nrOfAY8930);
+	printf("# YM3812: %u\n", preHeader.nrOfYM3812);
+	printf("# YMF262: %u\n", preHeader.nrOfYMF262);
+	printf("# MIDI: %u\n", preHeader.nrOfMIDI);
+	
+	// Save header
+	_farfwrite(&preHeader, sizeof(preHeader), 1, pOut);
+	
+	// Reset all pointers
+	for (i = 0; i < MAX_MULTICHIP; i++)
+		for (j = 0; j < NUM_CHIPS; j++)
+			pCommands[i][j] = commands[i][j] + 1;
+	
+	printf("Start preprocessing DRO\n");
+
+	// Process all register/data pairs in the file
+	while (!feof(pFile))//(lengthPairs < header.commands)
+	{
+		uint8_t data[2];
+		bool flush = 0;
+		
+		fread(data, sizeof(data), 1, pFile);
+		
+		// Handle special-case delay commands
+		if (data[0] == header.delay256)
+		{
+			// Delay 1-256 ms
+			// Accumulate delays until next command
+			totalDelay += (uint32_t)data[1] + 1;
+		}
+		else if (data[0] == header.delayShift8)
+		{
+			// Delay (1-256) << 8 ms
+			// Accumulate delays until next command
+			totalDelay += ((uint32_t)data[1] + 1) << 8;
+		}
+		else
+		{
+			uint8_t chip = 0;
+			uint16_t length, count;
+			
+			// Write delays first
+			// Break up into multiple delays with no notes
+			delay = 0;
+			if (totalDelay > 0)
+			{
+				// Convert to PIT ticks
+				delay = totalDelay*(PITFREQ/1000.0);
+				totalDelay = 0;
+			}
+			
+			while (delay > 0)
+			{
+				if (delay >= 65536L)
+				{
+					firstDelay = 0;
+					delay -= 65536L;
+				}
+				else
+				{
+					firstDelay = delay;
+					delay = 0;
+				}
+				
+				// First write delay value
+				fwrite(&firstDelay, sizeof(firstDelay), 1, pOut);
+				
+				// Now output commands
+				OutputCommands(pOut);
+				
+				// Reset command buffers
+				// (Next delays will get 0 notes exported
+				for (i = 0; i < MAX_MULTICHIP; i++)
+					for (j = 0; j < NUM_CHIPS; j++)
+						pCommands[i][j] = commands[i][j] + 1;
+			}
+			
+			// Normal register/data pair
+			if (data[0] & 0x80)
+			{
+				chip = 1;
+				data[0] &= 0x7F;
+			}
+			
+			if (header.hardware == 2)
+			{
+				// OPL3
+				if (!chip)
+				{
+					*pCommands[0][YMF262PORT0]++ = conversionTable[data[0]];
+					*pCommands[0][YMF262PORT0]++ = data[1];
+					
+					length = pCommands[0][YMF262PORT0] - commands[0][YMF262PORT0];
+					count = (length - 1) / 2;
+					
+					if (count >= 255)
+						flush = 1;
+				}
+				else
+				{
+					*pCommands[0][YMF262PORT1]++ = conversionTable[data[0]];
+					*pCommands[0][YMF262PORT1]++ = data[1];
+					
+					length = pCommands[0][YMF262PORT1] - commands[0][YMF262PORT1];
+					count = (length - 1) / 2;
+
+					if (count >= 255)
+						flush = 1;
+				}
+			}
+			else
+			{
+				// OPL2
+				*pCommands[chip][YM3812]++ = conversionTable[data[0]];
+				*pCommands[chip][YM3812]++ = data[1];
+				
+				length = pCommands[chip][YM3812] - commands[chip][YM3812];
+				count = (length - 1) / 2;
+
+				if (count >= 255)
+					flush = 1;
+			}
+			
+			if (flush)
+			{
+				printf("Flush!\n");
+				
+				// Flush if we have to many commands
+				firstDelay = 1;
+
+				// First write delay value
+				fwrite(&firstDelay, sizeof(firstDelay), 1, pOut);
+					
+				// Now output commands
+				OutputCommands(pOut);
+				
+				// Reset command buffers
+				// (Next delays will get 0 notes exported
+				for (i = 0; i < MAX_MULTICHIP; i++)
+					for (j = 0; j < NUM_CHIPS; j++)
+						pCommands[i][j] = commands[i][j] + 1;
+			}
+		}
+		
+		lengthPairs++;
+	}
+	
+	// Output last delay of 0
+	firstDelay = 0;
+
+	// Write to disk
+	fwrite(&firstDelay, sizeof(firstDelay), 1, pOut);
+	
+	// Output last set of commands
+	OutputCommands(pOut);
+	
+	// And a final delay of 0, which would get fetched by the last int handler
+	fwrite(&firstDelay, sizeof(firstDelay), 1, pOut);
+	
+	// Update size field
+	size = ftell(pOut);
+	size -= sizeof(preHeader);
+	fseek(pOut, 8, SEEK_SET);
+	fwrite(&size, sizeof(size), 1, pOut);
+
+	fclose(pOut);
+	
+	printf("Done preprocessing DRO\n");		
 }
 
 void SavePreprocessed(const char* pFileName)
