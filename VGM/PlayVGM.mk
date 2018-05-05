a9d04a4b08862f0565e0777400ff12328ff11272
Scali <none@none>
2018-05-05 15:09:39 +0200

    Don't track mk/mk1 files.

--- VGM/PlayVGM.mk
+++
@@ -1,3 +0,0 @@
-project : I:\Programming\Oldskool\DOS\VGM\PlayVGM.exe .SYMBOLIC
-
-!include I:\Programming\Oldskool\DOS\VGM\PlayVGM.mk1
--- VGM/VGMPlay.mk
+++
@@ -1,3 +0,0 @@
-project : I:\Programming\Oldskool\DOS\VGM\VGMPlay.exe .SYMBOLIC
-
-!include I:\Programming\Oldskool\DOS\VGM\VGMPlay.mk1
