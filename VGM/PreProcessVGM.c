7d488d1c3f0e0a3c3902a478048eea8b47638b96
Scali <scali@scali.eu.org>
2023-01-20 17:12:15 +0100

    Add extra pass to scan data for used chips, rather than rely on header data, which may not have clock rates for all chips used.

--- VGM/PreProcessVGM.c
+++ VGM/PreProcessVGM.c
@@ -81,6 +81,214 @@ void DetectChips(VGM_HEADER* pHeader, uint16_t dataOffset)
 	printf("# YM2608: %u\n", preHeader.nrOfYM2608);
 }
 
+void DetectChipsFromData(FILE* pFile)
+{
+	uint8_t playing = 1;
+	
+	// Detect used chips
+	preHeader.nrOfMIDI = 0;	// Not (yet?) supported in VGM
+	preHeader.nrOfSN76489 = 0;
+	preHeader.nrOfYM2151 = 0;	
+	preHeader.nrOfAY8930 = 0;
+	preHeader.nrOfYM3812 = 0;
+	preHeader.nrOfYMF262 = 0;
+	preHeader.nrOfSAA1099 = 0;
+	preHeader.nrOfYM2203 = 0;
+	preHeader.nrOfYM2608 = 0;
+	
+	while (playing)
+	{
+		uint8_t data[2];
+		DataBlock dataBlock;
+		uint8_t value, i;
+
+		value = fgetc(pFile);
+		
+		switch (value)
+		{
+			case 0x66:
+				// end of VGM data
+				playing = 0;
+				break;
+				
+			// Wait-commands
+			case 0x61:	// wait n samples
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0x62:	// wait 1/60th second: 735 samples
+			case 0x63:	// wait 1/50th second: 882 samples
+			case 0x70:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x71:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x72:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x73:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x74:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x75:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x76:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x77:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x78:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x79:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x7A:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x7B:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x7C:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x7D:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x7E:	// wait n+1 samples, n can range from 0 to 15.
+			case 0x7F:	// wait n+1 samples, n can range from 0 to 15.
+				break;
+
+				// SN76489 commands
+			case 0x4F:	// dd : Game Gear PSG stereo, write dd to port 0x06
+				if (preHeader.nrOfSN76489 < 1)
+					preHeader.nrOfSN76489 = 1;
+				fseek(pFile, 1, SEEK_CUR);
+				break;
+			case 0x3F: 	// dd : Second Game Gear PSG stereo, write dd to port 0x06
+				if (preHeader.nrOfSN76489 < 2)
+					preHeader.nrOfSN76489 = 2;
+				fseek(pFile, 1, SEEK_CUR);
+				break;
+			case 0x50:	// dd : PSG (SN76489/SN76496) write value dd
+				if (preHeader.nrOfSN76489 < 1)
+					preHeader.nrOfSN76489 = 1;
+				fseek(pFile, 1, SEEK_CUR);
+				break;
+			case 0x30:	// dd : Second PSG (SN76489/SN76496) write value dd
+				if (preHeader.nrOfSN76489 < 2)
+					preHeader.nrOfSN76489 = 2;
+				fseek(pFile, 1, SEEK_CUR);
+				break;
+			case 0x5A:	// aa dd : YM3812, write value dd to register aa
+				if (preHeader.nrOfYM3812 < 1)
+					preHeader.nrOfYM3812 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0xAA:	// aa dd : Second YM3812, write value dd to register aa
+				if (preHeader.nrOfYM3812 < 2)
+					preHeader.nrOfYM3812 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0x5E:	// aa dd : YMF262 port 0, write value dd to register aa
+				if (preHeader.nrOfYMF262 < 1)
+					preHeader.nrOfYMF262 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0x5F:	// aa dd : YMF262 port 1, write value dd to register aa
+				if (preHeader.nrOfYMF262 < 1)
+					preHeader.nrOfYMF262 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0xA0:	// aa dd : AY8910, write value dd to register aa
+				// Second chip is indicated by msb in first byte
+				fread(data, sizeof(data), 1, pFile);
+				i = (data[0] & 0x80) ? 2 : 1;
+				if (preHeader.nrOfAY8930 < i)
+					preHeader.nrOfAY8930 = i;
+				break;
+			case 0xBD:	// aa dd : SAA1099, write value dd to register aa
+				// Second chip is indicated by msb in first byte
+				fread(data, sizeof(data), 1, pFile);
+				i = (data[0] & 0x80) ? 2 : 1;
+				if (preHeader.nrOfSAA1099 < i)
+					preHeader.nrOfSAA1099 = i;
+				break;
+			case 0xAE:	// aa dd : Second YMF262 port 0, write value dd to register aa
+				if (preHeader.nrOfYMF262 < 2)
+					preHeader.nrOfYMF262 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0xAF:	// aa dd : Second YMF262 port 1, write value dd to register aa
+				if (preHeader.nrOfYMF262 < 2)
+					preHeader.nrOfYMF262 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+
+			case 0x54:	// aa dd : YM2151, write value dd to register aa
+				if (preHeader.nrOfYM2151 < 1)
+					preHeader.nrOfYM2151 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+			case 0xA4:	// aa dd : Second YM2151, write value dd to register aa
+				if (preHeader.nrOfYM2151 < 2)
+					preHeader.nrOfYM2151 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+		
+			case 0x55:	// aa dd : YM2203, write value dd to register aa
+				if (preHeader.nrOfYM2203 < 1)
+					preHeader.nrOfYM2203 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+				
+			case 0xA5:	// aa dd : Second YM2203, write value dd to register aa
+				if (preHeader.nrOfYM2203 < 2)
+					preHeader.nrOfYM2203 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+
+			case 0x56:	// aa dd : YM2608 port 0, write value dd to register aa
+				if (preHeader.nrOfYM2608 < 1)
+					preHeader.nrOfYM2608 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+
+			case 0x57:	// aa dd : YM2608 port 1, write value dd to register aa
+				if (preHeader.nrOfYM2608 < 1)
+					preHeader.nrOfYM2608 = 1;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+
+			case 0xA6:	// aa dd : Second YM2608 port 0, write value dd to register aa
+				if (preHeader.nrOfYM2608 < 2)
+					preHeader.nrOfYM2608 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+
+			case 0xA7:	// aa dd : Second YM2608 port 1, write value dd to register aa
+				if (preHeader.nrOfYM2608 < 2)
+					preHeader.nrOfYM2608 = 2;
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+				
+			case 0x67:	// Data block: skip for now
+				fread(&dataBlock, sizeof(dataBlock), 1, pFile);
+				fseek(pFile, dataBlock.dataSize, SEEK_CUR);
+				break;
+
+			case 0x51:	// aa dd : YM2413, write value dd to register aa
+			case 0xA1:	// aa dd : Second YM2413, write value dd to register aa
+			case 0x52:	// aa dd : YM2612 port 0, write value dd to register aa
+			case 0x53:	// aa dd : YM2612 port 1, write value dd to register aa
+			case 0xA2:	// aa dd : Second Second YM2612 port 0, write value dd to register aa
+			case 0xA3:	// aa dd : Second YM2612 port 1, write value dd to register aa
+			case 0x58:	// aa dd : YM2610 port 0, write value dd to register aa
+			case 0x59:	// aa dd : YM2610 port 1, write value dd to register aa
+			case 0xA8:	// aa dd : Second YM2610 port 0, write value dd to register aa
+			case 0xA9:	// aa dd : Second YM2610 port 1, write value dd to register aa
+			case 0x5B:	// aa dd : YM3526, write value dd to register aa
+			case 0xAB:	// aa dd : Second YM3526, write value dd to register aa
+			case 0x5C:	// aa dd : Y8950, write value dd to register aa
+			case 0xAC:	// aa dd : Second Y8950, write value dd to register aa
+			case 0x5D:	// aa dd : YMZ280B, write value dd to register aa
+			case 0xAD:	// aa dd : Second YMZ280B, write value dd to register aa
+				// Skip
+				fseek(pFile, 2, SEEK_CUR);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	printf("# SN76489: %u\n", preHeader.nrOfSN76489);
+	printf("# SAA1099: %u\n", preHeader.nrOfSAA1099);
+	printf("# AY8930: %u\n", preHeader.nrOfAY8930);
+	printf("# YM3812: %u\n", preHeader.nrOfYM3812);
+	printf("# YMF262: %u\n", preHeader.nrOfYMF262);
+	printf("# MIDI: %u\n", preHeader.nrOfMIDI);
+	printf("# YM2151: %u\n", preHeader.nrOfYM2151);
+	printf("# YM2203: %u\n", preHeader.nrOfYM2203);
+	printf("# YM2608: %u\n", preHeader.nrOfYM2608);
+}
+
 void PreProcessVGM(FILE* pFile, const char* pOutFile)
 {
 	FILE* pOut;
@@ -146,10 +354,14 @@ void PreProcessVGM(FILE* pFile, const char* pOutFile)
 	
 	printf("Start preprocessing VGM\n");
 	
+	//DetectChips(&header, dataOffset);
+	DetectChipsFromData(pFile);
+	
+	// Seek to VGM data
+	fseek(pFile, dataOffset, SEEK_SET);
+	
 	pOut = fopen(pOutFile, "wb");
-	
-	DetectChips(&header, dataOffset);
-	
+
 	// Save header
 	_farfwrite(&preHeader, sizeof(preHeader), 1, pOut);
 	
@@ -467,7 +679,6 @@ void PreProcessVGM(FILE* pFile, const char* pOutFile)
 			if (delay > 0)
 				printf("Very small delay detected: %lu!\n", delay);
 		}*/
-
 	}
 	
 	if (preHeader.loop != 0)
