4e35848bae16cf1e06b1a9a56d7dc7507935fb57
Scalibq <scali@scali.eu.org>
2023-01-28 20:42:38 +0100

    Remove COMMON folder.

--- COMMON/ASM/make.bat
+++
@@ -1,2 +0,0 @@
-tasm /m /ml 8259A.asm
-tasm /m /ml cmdline.asm
--- COMMON/C/OPL2LPT.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OPL2LPT_H_
-#define _OPL2LPT_H_
-
-#include <stdint.h>
-
-void WriteOPL2LPTAddr(uint16_t base, uint8_t val);
-void WriteOPL2LPTData(uint16_t base, uint8_t val);
-
-#endif /* _OPL2LPT_H_ */
